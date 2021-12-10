2c464863c9425aec28154c7ae190721af5685549
tuanhdo97 <82974540+tuanhdo97@users.noreply.github.com>
2021-12-09 23:48:41 -0500

    Update views.py

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -36,7 +36,7 @@ def login():
 
         user = User.query.filter_by(email=form.email.data).first()
 
-        if user.check_password(form.password.data) and user is not None:
+        if  user is not None and user.check_password(form.password.data):
 
             login_user(user)
             flash('Log in Success!')
